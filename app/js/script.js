d54252ed8a4bee79ed5218d8702b1497d523dada
alchimic <alchimicus@gmail.com>
2019-07-30 21:39:52 +0300

    fix items

--- app/js/script.js
+++ app/js/script.js
@@ -22,37 +22,54 @@ $(document).ready(function () {
 
 
 // Start Page cursors
-(function($) { "use strict";
-    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-    t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2")
-    function n(t) {
-        e.classList.add("hover")
-    }
-    function s(t) {
-        e.classList.remove("hover")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
+(function ($) {
+  "use strict";
+  document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+    t.style.left = n.clientX + "px",
+      t.style.top = n.clientY + "px",
+      e.style.left = n.clientX + "px",
+      e.style.top = n.clientY + "px"
+  });
+  var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2")
+
+  function n(t) {
+    e.classList.add("hover")
+  }
+
+  function s(t) {
+    e.classList.remove("hover")
+  }
+  s();
+  for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+    o(r[a])
+  }
+
+  function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+  }
 })(jQuery);
 // End Page cursors
 
 
 // Scroll Go
 let tl = new TimelineMax();
-tl.to('.header__picture', 1.65, { y: -200, scaleX:0.75, rotationX: 20, scaleY:0.75, opacity:0, ease:Linear.easeNone }, 0);
-tl.to('.header__logo img', 1, { y: -70, ease: Power4.easeOut }, 0);
-tl.to('.go', 1.85, { y: -400, ease:Linear.easeNone }, 0);
+tl.to('.header__picture', 1.65, {
+  y: -200,
+  scaleX: 0.75,
+  rotationX: 20,
+  scaleY: 0.75,
+  opacity: 0,
+  ease: Linear.easeNone
+}, 0);
+tl.to('.header__logo img', 1, {
+  y: -70,
+  ease: Power4.easeOut
+}, 0);
+tl.to('.go', 1.85, {
+  y: -400,
+  ease: Linear.easeNone
+}, 0);
 
 const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene({
@@ -63,15 +80,17 @@ const scene = new ScrollMagic.Scene({
 });
 
 
-scene.addIndicators({name: 'Lets Go'});
+scene.addIndicators({
+  name: 'Lets Go'
+});
 scene.setTween(tl);
 scene.addTo(controller);
 // End Scroll Go
 
 
 // Start Reveal effect
-(function($) {
-  $.expr[":"].onScreen = function(elem) {
+(function ($) {
+  $.expr[":"].onScreen = function (elem) {
     let $window = $(window)
     let viewport_top = $window.scrollTop()
     let viewport_height = $window.height()
@@ -82,22 +101,24 @@ scene.addTo(controller);
     let bottom = top + height
 
     return (top >= viewport_top && top < viewport_bottom) ||
-           (bottom > viewport_top && bottom <= viewport_bottom) ||
-           (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+      (bottom > viewport_top && bottom <= viewport_bottom) ||
+      (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
   }
 })(jQuery);
 
 
-$(function() {
-  setInterval(function() {
+$(function () {
+  setInterval(function () {
     $(".photo__item")
-      .filter(":onScreen") 
-      .addClass("focus-in") 
-      .addClass("reveal") 
-  })                             
+      .filter(":onScreen")
+      .addClass("focus-in")
+      .addClass("reveal")
+  })
 });
 // End Reveal effect
 
 
 
 window.console.log('Made with fun and love ❤️❤️❤️️ by Dmitriy Moskvichov')
+
+const g = 23;
