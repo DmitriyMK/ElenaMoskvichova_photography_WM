49e7f2354b931d08c77fa5c7081ea1a58558496a
alchimic <alchimicus@gmail.com>
2020-04-02 20:04:08 +0300

    fix images

--- app/js/libs/shaders — копия.js
+++ app/js/libs/shaders — копия.js
@@ -4,6 +4,25 @@
  * - Hover effect on the images: https://tympanus.net/codrops/2018/04/10/webgl-distortion-hover-effects/
  * - A PEN BY Francesco Michelini  https://codepen.io/kekkorider/pen/GRKqEXy
  */
+
+navigator.sayswho= (function(){
+  var ua= navigator.userAgent, tem, 
+  M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+      tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+      return 'IE '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+      tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+      if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+})();
+
+console.log(navigator.sayswho); // outputs: `Chrome 62`
+
 if (document.documentElement.clientWidth > 1024) {
   class App {
     constructor({ images = [], texts = [] } = {}) {
